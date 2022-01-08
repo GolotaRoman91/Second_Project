fa17496abb76938c9af54b0358ab17e9fd03dfff
Vladislav <vlad120686@gmail.com>
2022-01-08 06:32:17 +0200

    without range

--- src/script/filters.ts
+++ src/script/filters.ts
@@ -1,86 +1,84 @@
-import { DOM } from './dom'
-import { constants } from "./constans";
-import { domElement } from "./constans";
-import { filterData } from "./constans";
-import { variable } from "./constans";
+import { DOM } from './dom';
+import { constants } from './constans';
+import { domElement } from './constans';
+import { filterData } from './constans';
+import { variable } from './constans';
 import { creatFirstPage } from './showMovie';
-import { movie } from "./types";
+import { movie } from './types';
 
 export const showFilters = (): void => {
     domElement.containerFilter.style.display = 'block';
-}
+};
 
 export const getFilms = (event: Event): boolean => {
     if ((<HTMLElement>event.target).tagName !== 'LI') {
-        return false
+        return false;
     }
 
     if (!filterData.genre_ids.includes(+(<HTMLElement>event.target).id)) {
         filterData.genre_ids.push(+(<HTMLElement>event.target).id);
     }
-}
+};
 
 export const filterByiD = (evt: Event): void => {
     filterData.id = Number((<HTMLInputElement>evt.target).value);
-}
+};
 
 export const filterByLanguage = (evt: Event): void => {
     filterData.original_language = (<HTMLInputElement>evt.target).value;
-}
+};
 
 const handFiltGenre = (movies: movie[], genresList) => {
     const temporaryArr = [];
 
-    movies.forEach((movie) => {
-        genresList.forEach((genre) => {
+    movies.forEach(movie => {
+        genresList.forEach(genre => {
             if (movie.genre_ids.includes(genre) && !temporaryArr.includes(movie)) {
-                return temporaryArr.push(movie)
+                return temporaryArr.push(movie);
             }
-        })
-    })
+        });
+    });
 
-    return temporaryArr
-}
+    return temporaryArr;
+};
 
 const handleById = (movies: movie[], key: string, value: string): string[] => {
     const temporaryArr = [];
 
-    movies.forEach((movie) => {
+    movies.forEach(movie => {
         if (value === movie[key]) {
-            return temporaryArr.push(movie)
+            return temporaryArr.push(movie);
         }
-    })
+    });
 
-    return temporaryArr
-}
+    return temporaryArr;
+};
 
 export const showFilms = () => {
     const filtredByGenre = handFiltGenre(constants.movies, filterData.genre_ids);
     const filteredById = handleById(filtredByGenre.length ? filtredByGenre : constants.movies, 'id', filterData.id);
     const filteredByLang = handleById(filtredByGenre.length ? filtredByGenre : constants.movies, 'original_language', filterData.original_language);
 
-
     if (filteredById.length) {
         constants.filteredFilms = [...filteredById];
     } else if (filteredByLang.length) {
         constants.filteredFilms = [];
-        constants.filteredFilms = [...filteredByLang]
+        constants.filteredFilms = [...filteredByLang];
     } else if (filtredByGenre.length) {
         constants.filteredFilms = [];
-        constants.filteredFilms = [...filtredByGenre]
+        constants.filteredFilms = [...filtredByGenre];
     }
 
-    creatFirstPage(constants.filteredFilms, 0)
+    creatFirstPage(constants.filteredFilms, 0);
+    DOM.filter.classList.toggle('hidden');
+};
 
-    domElement.containerFilter.style.display = 'none';
+export function openCloseFilters() {
+    DOM.filter.classList.toggle('hidden');
 }
 
-export function openCloseFilters(evt: Event) {
-    DOM.filter.className === 'filter hidden' ? DOM.filter.className = 'filter' : DOM.filter.className = 'filter hidden';
-}
-
-export function changeColorGenres(evt: Event) {
-    ((<HTMLElement>evt.target).style.color !== 'red') ? (<HTMLElement>evt.target).style.color = 'red' : (<HTMLElement>evt.target).style.color = 'white';
+export function changeColorGenres(evt: any) {
+    evt.target.classList.toggle('filmGenresActiv');
 }
 
 export const resetFilter = (): void => {
@@ -89,10 +87,19 @@ export const resetFilter = (): void => {
     filterData.release_date = null;
     filterData.original_language = null;
     filterData.budget = null;
-    filterData.adult = null
+    filterData.adult = null;
 
     variable.skip = 0;
-    constants.filteredFilms = []
-    creatFirstPage(constants.movies, 0)
+    constants.filteredFilms = [];
+    creatFirstPage(constants.movies, 0);
     domElement.containerFilter.style.display = 'none';
+    DOM.filter.classList.toggle('hidden');
+    clearFiltersGenres();
+};
+
+function clearFiltersGenres() {
+    const genresList = Array.from(document.querySelectorAll('.filmsGenres'));
+    genresList.forEach(element => {
+        element.classList.remove('filmGenresActiv');
+    });
 }
