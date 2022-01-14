8f4e84ee1518c96fd7ae16ff3388abeec9b49c72
Vladislav <vlad120686@gmail.com>
2022-01-14 03:14:18 +0200

    try_catch, const url_sign, type ts

--- src/script/filters.ts
+++ src/script/filters.ts
@@ -2,7 +2,7 @@ import { DOM } from './dom';
 import { constants, URL, filterData, variable } from './constans';
 import { addMovie } from './showMovie';
 export async function getFiltredFilms() {
-    let query = URL.movies;
+    let query = URL.constMovies;
     if (DOM.selectLanguage.value !== 'All') {
         query += `languages=${DOM.selectLanguage.value}&`;
     }
@@ -72,8 +72,8 @@ export const resetFilter = (): void => {
     (<HTMLInputElement>document.querySelector('.dual-range')).style = '--x-1:-9.34375px; --x-2:500px';
     clearFiltersGenres();
     (<HTMLInputElement>document.querySelector('#selectLanguage')).value = 'All';
-    DOM.minCalendarData.value =  DOM.minCalendarData.min;
-    DOM.maxCalendarData.value =  new Date().toLocaleDateString('sv-se');
+    DOM.minCalendarData.value = DOM.minCalendarData.min;
+    DOM.maxCalendarData.value = new Date().toLocaleDateString('sv-se');
 };
 function clearFiltersGenres() {
     const genresList = Array.from(document.querySelectorAll('.filmsGenres'));
